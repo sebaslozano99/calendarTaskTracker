f62dbd6827ca9022dd920a3f58d9ac43e1d548f8
sebastian lozano <sebaslozano708@gmail.com>
2024-09-06 20:17:25 -0500

    calendar up and running. Fixed bugs

--- index.js
+++ index.js
@@ -15,19 +15,18 @@ const calendarContainer = document.querySelector(".calendarContainer__dayNumbers
 const currentDate = new Date(); // We get current date
 let year = currentDate.getFullYear(); // current year 
 let month = currentDate.getMonth(); // current month
+let day = currentDate.getDate();
 let dateToDisplay = new Date(year, month); // current date starting from the 1st day of the month based on the currentDate above
 let currentMonthFirstDayName = dateToDisplay.toDateString().split(" ")[0]; //first 3 letters of the current day "Sun" for "Sunday"
-let indexOfDay = indexOfDayName(currentMonthFirstDayName); // number associated to name of the day
+let indexOfDay = indexOfDayName(currentMonthFirstDayName); // number associated to name of the first day of the month
 let daysCurrentMonth = getAllDaysInTheMonth(); //we track how many days are in the current months
 
 
 console.log("currentDate: ", currentDate);
-console.log("year: ", year);
-console.log("month: ", month);
 console.log("dateToDisplay: ", dateToDisplay);
 console.log("current month first day name: ", currentMonthFirstDayName);
 console.log("number of days in current month: ", daysCurrentMonth);
-
+console.log(`index of the 1st day: ${indexOfDay}`);
 
 
 
@@ -39,7 +38,7 @@ prevBtn.addEventListener("click", prevMonth);
 
 
 
-console.log(month, dateToDisplay.getMonth());
+
 
 // ----- HELPER FUNCTIONS -----
 
@@ -49,13 +48,14 @@ function renderUI(){
     const monthToDisplay = displayCorrectMonth(month);
     monthAndYear.textContent = `${monthToDisplay} ${year}`;
 
-
+    // clear previous HTML code
     calendarContainer.innerHTML = "";
+    
     let html = "";
 
     daysCurrentMonth.forEach(element => {
         const segment = `
-            <div class="dayNumbers__day">${element}</div>
+            <div class="dayNumbers__day ${element === day && month === currentDate.getMonth() && "currentDay"}">${element}</div>
         `;
         html += segment;
 
@@ -64,6 +64,10 @@ function renderUI(){
     calendarContainer.innerHTML = html;
     html = ""; 
 }
+
+
+
+
 
 function nextMonth(){
     if(month < 11){
@@ -74,22 +78,14 @@ function nextMonth(){
         year++;
     }
 
-    console.log(month, dateToDisplay.getMonth());
-    dateToDisplay = new Date(year, month);
-    indexOfDay = indexOfDayName(currentMonthFirstDayName);
-    daysCurrentMonth = getAllDaysInTheMonth(); //we track how many days are in the current months
-    currentMonthFirstDayName = dateToDisplay.toDateString().split(" ")[0]; //first 3 letters of the current day "Sun" for "Sunday"
-    renderUI();
-
-    console.log("currentDate: ", currentDate);
-    console.log("year: ", year);
-    console.log("month: ", month);
-    console.log("dateToDisplay: ", dateToDisplay);
-    console.log("current month first day name: ", currentMonthFirstDayName);
-    console.log("number of days in current month: ", daysCurrentMonth);
-    console.log("index of day: ", indexOfDay);
+    updateUI();
+    console.log(`index of the 1st day: ${indexOfDay}`);
 
 }
+
+
+
+
 
 function prevMonth(){
     if(month > 0){
@@ -99,9 +95,10 @@ function prevMonth(){
         month = 11;
         year--;
     }
-    renderUI();
-    dateToDisplay = new Date(year, month);
-    console.log(dateToDisplay);
+
+    updateUI();
+    console.log(`index of the 1st day: ${indexOfDay}`);
+
 }
 
 
@@ -110,6 +107,29 @@ function prevMonth(){
 
 // ----- OTHER FUNCTIONS -----
 
+
+function updateUI(){
+    // update this based on changes on year and/or month
+    dateToDisplay = new Date(year, month);
+
+    //get the name of the first day on the new month
+    currentMonthFirstDayName = dateToDisplay.toDateString().split(" ")[0]; //first 3 letters of the current day "Sun" for "Sunday"
+
+    // turn the name of the first day on the new month to an index
+    indexOfDay = indexOfDayName(currentMonthFirstDayName);
+
+    //count how many days are in the current month
+    daysCurrentMonth = getAllDaysInTheMonth();
+
+    //render UI
+    renderUI();
+
+}
+
+
+
+
+
 function displayCorrectMonth(numberOfMonth = 0){
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Octuber", "November", "December"];
 
@@ -117,17 +137,20 @@ function displayCorrectMonth(numberOfMonth = 0){
 }
 
 
-// While the dateToDisplay's month is equal to month, we will increase the day of the "dateToDisplay" until it changes to the next month
+
+
+
+// While the dateToDisplay's month is equal to month, we will increase the day of the "dateToDisplay" until it changes to the next month -----
+
 function getAllDaysInTheMonth(){
     const array = [];
     let index = 0;
-    // const indexOfDay = indexOfDayName(currentMonthFirstDayName); // number associated to name of the day
     
 
-    //If the index of the day's name is greater than 0, it means the 1st day of the month fall after sunday. Thus, tho display it correctly in the UI, we'll add en empty string, so that in the UI that will reflect as an empty box.
+    //If the index of the day's name is greater than 0, it means the 1st day of the month fall after sunday. Thus, tho display it correctly in the UI, we'll add en empty string, so that in the UI that will reflect as an empty box. 
     if(indexOfDay > 0) {
         for(let i = 0; i < indexOfDay; i++){
-            console.log("--", i);
+            // console.log("--", i);
             array.push("");
         }
     }
@@ -137,12 +160,16 @@ function getAllDaysInTheMonth(){
         index++;
         array.push(index);
     }
-    dateToDisplay = new Date(year, month); 
+    dateToDisplay.setMonth(month);
     return array;
 }
 
 
-//We associate the name of the day with an index
+
+
+
+//We associate the name of the day with an index -----
+
 function indexOfDayName(dayName){
     const names = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
     return names.indexOf(dayName.toLowerCase());
